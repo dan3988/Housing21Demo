82b33fb48ea2e3cdafdf6790e9ad815b41105709
dan3988 <40858622+dan3988@users.noreply.github.com>
2023-08-27 15:06:28 +0100

    Use datatables.net to add sorting & pagination to the users table

---
+++ Demo.Website/wwwroot/js/index.js
@@ -0,0 +1,53 @@
+﻿/// <reference types="jquery"/>
+/// <reference types="datatables.net"/>
+async function deleteUser(id) {
+	const res = await fetch(`/api/Users/${id}`, {
+		method: "DELETE"
+	});
+
+	if (res.ok) {
+		location.reload();
+	} else {
+		alert("Failed to delete user");
+	}
+}
+
+/**
+ * 
+ * @param {HTMLElement} parent
+ * @param {string} href
+ * @param {string} text
+ */
+function addLink(parent, href, text) {
+	const a = document.createElement("a");
+	a.className = "btn btn-outline-primary";
+	a.href = href;
+	a.innerText = text;
+	parent.appendChild(a);
+}
+
+var dt = $("#table").DataTable({
+	searching: false,
+	columns: [
+		{
+		},
+		{
+		},
+		{
+		},
+		{
+		},
+		{
+		},
+		{
+			searchable: false,
+			orderable: false
+		},
+	],
+	dom: '<"top d-flex gap-1 pb-1"l<"flex-fill">fr>t<"bottom"ip>'
+});
+
+$("#table_wrapper .top").each((i, e) => {
+	addLink(e, "/Edit", "Add User");
+	addLink(e, "/api/Users/csv", "Download");
+});
